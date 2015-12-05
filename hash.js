8de0eda417f5531598390c95f1f0f815bf49121d
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-05 17:58:16 +0400

    free

--- hash.js
+++ hash.js
@@ -1,6 +1,7 @@
-(function () {
+define('?*hash/hash.js', function () {
 	var hash = function (args) {
 		return JSON.stringify(args);
 	}
 	window.hash=hash;
-})();
+	return hash
+});
--- infra.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $infra;
-infra_when($infra, 'oninitjs', function () {
-	global $infra;
-	$infra['js'] .= $infra['require']('*hash/hash.js');
-});
