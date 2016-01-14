87b156e0267768e5d1c96cfd2af470b82ed50794
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-01-15 00:58:21 +0400

    js start

---
+++ Hash.js
@@ -0,0 +1,8 @@
+(function() {
+	var Hash = {
+		exec: function (args) {
+			return JSON.stringify(args);
+		}
+	}
+	window.Hash=Hash;
+})();
--- hash.js
+++
@@ -1,4 +0,0 @@
-
-infrajs.hash = function (args) {
-	return JSON.stringify(args);
-}
